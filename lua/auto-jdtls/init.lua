0a18df44a22c54a9720dfd934847421b7a5a52ac
Pojok Code <asep.komarudin@dataon.com>
2024-11-16 13:46:21 +0700

    fix: on load by file type

--- lua/auto-jdtls/init.lua
+++ lua/auto-jdtls/init.lua
@@ -3,10 +3,8 @@ local M = {}
 M.setup = function(opt)
   opt = opt or {}
   require("auto-jdtls.create_maven_project")
-  if vim.bo.filetype == "java" then
-    require("auto-jdtls.utils").install()
-    require("auto-jdtls.utils").attach_jdtls(opt)
-  end
+  require("auto-jdtls.utils").install()
+  require("auto-jdtls.utils").attach_jdtls(opt)
 end
 
 return M
